b7281e573df732c80565876688ffab5b5bbf789b
Shibodd <simonebondi456@gmail.com>
2023-10-14 19:51:43 +0200

    Added benchmark spec file... spec

---
+++ benchmarking/benchmark_spec.py
@@ -0,0 +1,39 @@
+import json
+from dataclasses import dataclass
+
+@dataclass
+class BenchmarkQuerySpec:
+  main_query: str
+  sentiment_query: str
+  dataset: list[tuple[str, int]]
+
+
+""" 
+// Example benchmark spec
+{
+  "queries": [
+    {
+      "main_query": "Gesu' cristo",
+      "sentiment_query": "disgust AND anger",
+      "dataset": [
+        [ "dm75og5", 3 ],
+        [ "fak1rr9", 2 ]
+      ]
+    }
+  ]
+}
+"""
+
+def read_benchmark_spec(path):
+  with open(path, 'r', encoding='utf8') as f:
+    spec = json.load(f)
+
+  return [
+    BenchmarkQuerySpec(
+      query['main_query'],
+      query['sentiment_query'],
+      [ (entry[0], entry[1]) for entry in query['dataset'] ]
+    )
+    for query in spec['queries']
+  ]
+
