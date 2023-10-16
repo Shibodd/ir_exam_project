83eac11cb09f0e2a5748adcef8d8f6372ad31a36
Shibodd <simonebondi456@gmail.com>
2023-10-16 12:56:57 +0200

    Add set_index() to SearchEngine

--- app/searching.py
+++ app/searching.py
@@ -2,9 +2,11 @@ import sentiment
 from app.query_parsing import parse_query
 
 class SearchEngine:
-  def __init__(self, index, weighting_model=sentiment.sentiment_scoring.SentimentWeightingModel()):
-    self.index = index
+  def __init__(self, weighting_model=sentiment.sentiment_scoring.SentimentWeightingModel()):
     self.weighting_model = weighting_model
+
+  def set_index(self, index):
+    self.index = index
 
   def search(self, main_query, sentiment_query = None):
     query, sentiment_vector = parse_query(self.index.schema, main_query, sentiment_query)
--- my_search.py
+++ my_search.py
@@ -1,11 +1,7 @@
 from app.searching import SearchEngine
-from whoosh.index import open_dir
+import whoosh.index
 
-index = open_dir("indexdir")
-searcher = SearchEngine(index)
-
-from benchmarking import benchmark, benchmark_spec
-benchmark.benchmark(searcher, benchmark_spec.read_benchmark_spec('benchmarking/benchmark.json'))
-
-# for result in searcher.search('title:"Cyka blyat" episode:69', 'fear'):
-#  print(result)
+searcher = SearchEngine()
+searcher.set_index(whoosh.index.open_dir("indexdir"))
+for result in searcher.search('title:"Cyka blyat" episode:69', 'fear'):
+  print(result)
