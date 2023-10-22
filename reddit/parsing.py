059082f7adaaabdc67720cd5e621d1a859e21d9d
Shibodd <simonebondi456@gmail.com>
2023-10-22 12:42:17 +0200

    Strip markdown_to_plaintext output

--- reddit/parsing.py
+++ reddit/parsing.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def markdown_to_plaintext(text):
   __md = Markdown(output_format="html")
   __md.stripTopLevelTags = False
-  return BeautifulSoup(__md.convert(text), 'html.parser').get_text()
+  return BeautifulSoup(__md.convert(text), 'html.parser').get_text().strip()
 
 def parse_post_title(title: str):
   try:
