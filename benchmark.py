03fe242e86fbbbc87566c93d696e7e23376b1dd1
Shibodd <simonebondi456@gmail.com>
2023-10-26 12:03:24 +0200

    Moved visualization of benchmark results. Use new average function. Fix typo, renaming idcg_ub to idcg_lb

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,16 @@ import whoosh.index
 import app
 import benchmarking
 
+import common_utils.arrays
+import numpy as np
+
+
+def visualize_single_query_results(dcg: np.ndarray, idcg_lb: np.ndarray):
+  print(dcg, idcg_lb)
+
+def visualize_average_results(dcg: np.ndarray, idcg_lb: np.ndarray):
+  print(dcg, idcg_lb)
+
 
 def run_interactive_benchmark_query(searcher: app.SearchEngine, bqm: benchmarking.BenchmarkQueryManager, limit=50):
   """
@@ -42,26 +52,32 @@ def run_interactive_benchmark(index_dir, benchmark_dir):
   searcher.set_index(index)
 
   dcg_by_query = []
-  idcg_ub_by_query = []
+  idcg_lb_by_query = []
 
   for query_file in benchmark_dir.glob('*.json'):
     with benchmarking.BenchmarkQueryManager(query_file) as bqm:
       # The relevance score of each result
       scores = run_interactive_benchmark_query(searcher, bqm)
 
-      # Upper bound for the Ideal DCG for this query
-      query_idcg_ub = benchmarking.dcg.compute_ideal_DCG(bqm.iter_scores())
+      # Lower bound for the Ideal DCG for this query
+      # (it's very likely that we didn't rate all relevant documents!)
+      query_idcg_lb = benchmarking.dcg.compute_ideal_DCG(bqm.iter_scores())
     
     # Discounted Cumulative Gain for this query
     query_dcg = benchmarking.dcg.compute_DCG(scores)
 
     dcg_by_query.append(query_dcg)
-    idcg_ub_by_query.append(query_idcg_ub)
+    idcg_lb_by_query.append(query_idcg_lb)
     
     # TODO: Fare un output decente per mostrare i risultati
-    print(query_dcg, idcg_ub_by_query)
+    visualize_single_query_results(query_dcg, query_idcg_lb)
 
-  # Compute the average DCG and IDCG upper bound over all the queries of this benchmark
+  # Compute the average DCG and IDCG lower bound over all the queries of this benchmark
+  average_dcg = common_utils.arrays.average(dcg_by_query)
+  average_idcg_lb = common_utils.arrays.average(idcg_lb_by_query)
+
+  visualize_average_results(average_dcg, average_idcg_lb)
+
   
 
 import argparse
