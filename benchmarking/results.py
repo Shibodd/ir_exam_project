75bd53e6e81538eb88aea3e61f1f70a2a39bfd22
Michele145987 <275018@studenti.unimore.it>
2023-10-31 16:51:44 +0100

    Adding class QueryResults e output comment before rating

--- benchmarking/results.py
+++ benchmarking/results.py
@@ -2,8 +2,14 @@ from dataclasses import dataclass
 import numpy as np
 
 @dataclass
+class QueryResults:
+  dcg: np.ndarray
+  idcg_lb: np.ndarray
+  main_query: str
+  sentiment_query: str
+
+@dataclass
 class BenchmarkResults:
-  dcg_by_query: list[np.ndarray]
-  idcg_lb_by_query: list[np.ndarray]
+  results_by_query: list[QueryResults]
   average_dcg: np.ndarray
   average_idcg_lb: np.ndarray
