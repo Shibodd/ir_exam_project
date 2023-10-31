8d5702bae5f01464cd9931daf15bce0459703e75
Shibodd <simonebondi456@gmail.com>
2023-10-31 15:42:36 +0100

    Schema V2

--- sentiment/schema.py
+++ sentiment/schema.py
@@ -1,4 +1,4 @@
-from whoosh import fields, analysis, formats
+from whoosh import fields, analysis, formats, reading, writing
 
 class SentimentField(fields.FieldType):
   def __init__(self):
@@ -13,7 +13,7 @@ class SentimentField(fields.FieldType):
     self.set_sortable(False)
 
 
-SCHEMA = fields.Schema(
+SCHEMA_V1 = fields.Schema(
   content=fields.TEXT(stored=True),
   sentiment=SentimentField(),
   title=fields.TEXT(stored=True),
@@ -21,3 +21,18 @@ SCHEMA = fields.Schema(
   comment_id=fields.ID(stored=True, unique=True),
   submission_id=fields.ID(stored=True, unique=True)
 )
+
+# Changed analyzer for content
+SCHEMA_V2 = fields.Schema(
+  content=fields.TEXT(stored=True, analyzer=analysis.LanguageAnalyzer('en')),
+  sentiment=SentimentField(),
+  title=fields.TEXT(stored=True, analyzer=analysis.RegexTokenizer()),
+  episode=fields.NUMERIC(stored=True),
+  comment_id=fields.ID(stored=True, unique=True),
+  submission_id=fields.ID(stored=True, unique=True)
+)
+
+def schema_v1_to_schema_v2(index_v1: reading.IndexReader, index_v2: writing.IndexWriter):
+  # Just need to run the analyzer - copy everything
+  for _, stored in index_v1.iter_docs():
+    index_v2.add_document(**stored) 
