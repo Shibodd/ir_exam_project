fb864b406bbb35da7942b27ee5c14bcf4e44278e
Shibodd <simonebondi456@gmail.com>
2023-11-01 08:26:35 +0100

    Move index creator run to index_creator.run

--- async_index_creator.py
+++ async_index_creator.py
@@ -1,26 +1,16 @@
-import index_creator
+import argparse
+import index_creator.run
 import asyncio
 import logging
 
 import logging
-import os
-import sentiment.schema
-import whoosh.index
 import asyncpraw
-import argparse
+
 import reddit
 
 logger = logging.getLogger(__name__)
 
 async def run_index_creator(index_directory):
-  if not os.path.exists(index_directory):
-    logger.info("Creating a new index...")
-    os.mkdir(index_directory)
-    index = whoosh.index.create_in(index_directory, sentiment.schema.SCHEMA)
-  else:
-    logger.info("Opening pre-existing index...")
-    index = whoosh.index.open_dir(index_directory)
-
   logger.info("Connecting to Reddit...")
   async with asyncpraw.Reddit(
       client_id="fyFBUMdaXtoogBzfHs-FQg",
@@ -32,25 +22,9 @@ async def run_index_creator(index_directory):
 
     logger.info("Downloading archive...")
     submission_ids = await reddit.submission_archive.get_submissions_for_years(red, [2022, 2021, 2020, 2019, 2018, 2017, 2016])
+    await index_creator.run.run(red, index_directory, submission_ids)
 
-    logger.info("Starting")
-    queue = asyncio.Queue(400)
-    with index_creator.IndexManager(index) as index_manager:
-      task1 = asyncio.create_task(index_creator.CommentProducer(index_manager, queue).run(red, submission_ids))
-      task2 = asyncio.create_task(index_creator.CommentConsumer(index_manager, queue, 200).run())
-      
-      # The tasks handle cancellation gracefully
-      while True:
-        try:
-          await asyncio.gather(task1)
-          task2.cancel()
-          await asyncio.gather(task2) 
-          return
-        except asyncio.CancelledError:
-          pass
-
-
-async def main():
+if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("index_directory", help="The directory in which the index will be created / updated.")
   args = parser.parse_args()
@@ -69,12 +43,7 @@ async def main():
   logging.getLogger('async_index_creator').setLevel(logging.DEBUG)
   logging.getLogger('').setLevel(logging.DEBUG)
 
-  await run_index_creator(args.index_directory)
-  return 0
-
-
-if __name__ == '__main__':
   try:
-    exit(asyncio.run(main()))
+    exit(asyncio.run(run_index_creator(args.index_directory)))
   except KeyboardInterrupt:
     exit(0)
---
+++ index_creator/run.py
@@ -0,0 +1,36 @@
+import index_creator
+import asyncio
+import logging
+
+import logging
+import os
+import sentiment.schema
+import whoosh.index
+import asyncpraw
+
+logger = logging.Logger(__name__)
+
+async def run(red: asyncpraw.Reddit, index_directory, submission_ids: list[str]):
+  if not os.path.exists(index_directory):
+    logger.info("Creating a new index...")
+    os.mkdir(index_directory)
+    index = whoosh.index.create_in(index_directory, sentiment.schema.SCHEMA)
+  else:
+    logger.info("Opening pre-existing index...")
+    index = whoosh.index.open_dir(index_directory)
+
+  logger.info("Starting")
+  queue = asyncio.Queue(400)
+  with index_creator.IndexManager(index) as index_manager:
+    task1 = asyncio.create_task(index_creator.CommentProducer(index_manager, queue).run(red, submission_ids))
+    task2 = asyncio.create_task(index_creator.CommentConsumer(index_manager, queue, 200).run())
+    
+    # The tasks handle cancellation gracefully
+    while True:
+      try:
+        await asyncio.gather(task1)
+        task2.cancel()
+        await asyncio.gather(task2) 
+        return
+      except asyncio.CancelledError:
+        pass
