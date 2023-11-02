b2a830ed889b70d7a946dbad111718700fcb82f6
Shibodd <simonebondi456@gmail.com>
2023-11-02 17:26:05 +0100

    Delete unused scripts

--- my_search.py
+++
@@ -1,9 +0,0 @@
-from app.searching import SearchEngine
-import whoosh.index
-
-searcher = SearchEngine()
-searcher.set_index(whoosh.index.open_dir("indexdir"))
-for result in searcher.search('Azudra', 'sadness AND NOT surprise'):
-  # print("\n\n\n")
-  # print(result['content'])
-  print(f"{result.score:.2f} {result['sentiment'].human_readable()}")
